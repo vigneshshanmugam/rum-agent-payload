e3c259cc08dd95f7ae669e97ecd2921801b5245c
vigneshshanmugam <vignesh.shanmugam22@gmail.com>
2020-01-13 15:23:37 +0100

    use compress-payload package

--- examples/utils.js
+++ examples/utils.js
@@ -1,4 +1,5 @@
 import { init, apm } from "../web_modules/@elastic/apm-rum.js";
+import { compress, decompress, view } from "../web_modules/compress-payload.js";
 
 const perfMonitoring = apm.serviceFactory.getService("PerformanceMonitoring");
 const apmServer = apm.serviceFactory.getService("ApmServer");
@@ -80,11 +81,18 @@ export function jsonTransactions(transactions) {
 }
 
 export async function renderPayloadSize(...args) {
+  const payload = args[1];
+
+  const c = compress(payload);
+  const { size: bytes } = await view(c);
+  console.log("Compresssed Size", bytes);
+
   const [metadata, transaction, ...spansAndBreakdown] = args[1].split("\n");
   const result = await originalPostJson.apply(apmServer, args);
   const { message, size } = JSON.parse(result);
   document.body.innerHTML = `
       <h2>${message}: ${size} bytes</h2>
+      <h3 style="color: grey">Payload size using Compression Stream: ${bytes} bytes</h3>
       <div>
          <h3> Metadata </h2>
           ${metadata}
---
+++ web_modules/compress-payload.js
@@ -0,0 +1,64 @@
+const encoder = new TextEncoder();
+const decoder = new TextDecoder();
+
+function stringToBuffer(str) {
+  return encoder.encode(str);
+}
+
+function bufferToString(buffer) {
+  return decoder.decode(buffer);
+}
+
+function compress(payload, type = "gzip") {
+  if (typeof payload !== "string") {
+    throw new Error("Payload must be in string format");
+  }
+
+  const cs = new CompressionStream(type);
+  const buffer = stringToBuffer(payload);
+  const writer = cs.writable.getWriter();
+  writer.write(buffer);
+  writer.close();
+
+  return cs;
+}
+
+async function decompress(cs, type = "gzip") {
+  const creader = cs.readable.getReader();
+  const ds = new DecompressionStream(type);
+  const dwriter = ds.writable.getWriter();
+  while (true) {
+    const { value, done } = await creader.read();
+    if (done) break;
+    dwriter.write(value);
+  }
+  dwriter.close();
+  return ds;
+}
+
+async function view(stream) {
+  const reader = stream.readable.getReader();
+  const chunks = [];
+  let totalSize = 0;
+
+  while (true) {
+    const { value, done } = await reader.read();
+    if (done) break;
+    chunks.push(value);
+    totalSize += value.byteLength;
+  }
+
+  const concatenate = new Uint8Array(totalSize);
+  let offset = 0;
+  for (const chunk of chunks) {
+    concatenate.set(chunk, offset);
+    offset += chunk.byteLength;
+  }
+
+  return {
+    size: totalSize,
+    contents: bufferToString(concatenate)
+  };
+}
+
+export { bufferToString, compress, decompress, stringToBuffer, view };
